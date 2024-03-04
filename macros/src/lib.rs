8323bdbb0165a09d6c5fc50662f2af40c8956df0
dommyrock <dominik.polzer@hotmail.com>
2024-03-04 18:13:16 +0100

    update proc macro code

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,27 +1,29 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse::Parse, parse_macro_input, spanned::Spanned, ItemFn};
+use syn::{parse::Parse, parse_macro_input, spanned::Spanned, Ident, ItemFn};
 
 #[derive(Debug)]
-enum Time { 
+enum Time {
     Second,
     Mili,
-    Nano
+    Micro,
+    Nano,
 }
 
-impl Parse for Time{
+impl Parse for Time {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let path: syn::Path = input.parse()?;
         let ident = path.segments.last().unwrap().ident.to_string();
         match ident.as_str() {
             "Second" => Ok(Time::Second),
             "Mili" => Ok(Time::Mili),
+            "Micro" => Ok(Time::Micro),
             "Nano" => Ok(Time::Nano),
             _ => Err(syn::Error::new(path.span(), "Invalid time value")),
         }
     }
 }
-//v2 parse string inputs 
+//v2 parse string inputs
 //ie. #[timed("Second")]]
 // impl Parse for Time {
 //     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
@@ -35,7 +37,6 @@ impl Parse for Time{
 //     }
 // }
 
-
 #[proc_macro_attribute]
 pub fn timed(args: TokenStream, input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
@@ -45,9 +46,19 @@ pub fn timed(args: TokenStream, input: TokenStream) -> TokenStream {
     let args = parse_macro_input!(args as Time);
 
     let unit = match args {
-        Time::Second => { quote! { as_secs }},
-        Time::Mili => { quote! { as_millis }},
-        Time::Nano => { quote! { as_nanos }},
+        Time::Second => {quote! { as_secs }}
+        Time::Mili => {quote! { as_millis }}
+        Time::Micro => {quote! { as_micros }}
+        Time::Nano => {quote! { as_nanos }}
+    };
+
+    let input = syn::parse2::<Ident>(unit.clone()).unwrap();
+    let stringified = match input.to_string().as_str() {
+        "as_secs" => "seconds",
+        "as_millis" => "ms",
+        "as_micros" => "microseconds",
+        "as_nanos" => "nanoseconds",
+        _ => "",
     };
 
     // Modify the function
@@ -56,10 +67,10 @@ pub fn timed(args: TokenStream, input: TokenStream) -> TokenStream {
         let start = std::time::Instant::now();
         let result = #code_block;
         let elapsed = start.elapsed();
-        println!("Time elapsed: {} {:?}", elapsed.#unit(), stringify!(#unit));
+        print!("Time elapsed: {:.2} {}", elapsed.as_secs_f32() * 1_000_000.0, "µs (microseconds) | ",);
+        println!("{} {:?}", elapsed.#unit(), #stringified);
         result
     });
 
-    // Return the modified function
     TokenStream::from(quote! { #function })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use macros::timed;
 pub enum Time {
     Second,
     Mili,
+    Micro,
     Nano,
 }
 
@@ -24,39 +25,23 @@ fn t1() {
     assert_eq!(a, [1, 2, 2, 3, 5, 6])
 }
 
+
 #[test]
+#[timed(Time::Nano)]
 fn t2() {
     let mut a: Vec<i32> = vec![1, 2, 3, 0, 0, 0];
     let b: Vec<i32> = vec![2, 5, 6];
-    let mut result = vec![0; a.len() + b.len()];
 
-    let mut result = vec![0; a.len() + b.len()];
-    let (mut i, mut j, mut k) = (0, 0, 0);
+    let start_idx = a.len() - b.len();
 
-    while i < a.len() && j < b.len() {
-        if a[i] < b[j] {
-            result[k] = a[i];
-            i += 1;
-        } else {
-            result[k] = b[j];
-            j += 1;
+    for i in 0..b.len() {
+        unsafe {
+            *a.get_unchecked_mut(start_idx + i) = *b.get_unchecked(i);
         }
-        k += 1;
     }
 
-    while i < a.len() {
-        result[k] = a[i];
-        i += 1;
-        k += 1;
-    }
-
-    while j < b.len() {
-        result[k] = b[j];
-        j += 1;
-        k += 1;
-    }
-    a = result;
-    assert_eq!(a, [1, 2, 2, 3, 5, 6])
+    a.sort_unstable();
+    assert_eq!(a, [1, 2, 2, 3, 5, 6]);
 }
 
 //ADD CRITERION TO GROUP ALL THE TESTS IN CURRENT FILE INTO GROUP ... 
